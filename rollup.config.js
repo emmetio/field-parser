ef896756670a1b616eb0a354911ff4251958bf82
Sergey Chikuyonok <serge.che@gmail.com>
2017-03-23 02:53:08 +0300

    Updated Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 export default {
 	entry: './index.js',
 	exports: 'named',
-	external: ['@emmetio/stream-reader'],
+	external: [
+		'@emmetio/stream-reader',
+		'@emmetio/stream-reader-utils'
+	],
 	targets: [
 		{format: 'cjs', dest: 'dist/field-parser.cjs.js'},
 		{format: 'es',  dest: 'dist/field-parser.es.js'}
